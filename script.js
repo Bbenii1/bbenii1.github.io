5048119fe5bf4fd90880220656acdf00af94285d
Szlonkai Benedek <szlonkaibenedek@gmail.com>
2024-10-23 17:04:59 +0200

    design overhaul

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-const split = id => {
-    const element = document.getElementById(id),
-            text = element.innerText.split("");
-
-    element.innerText = "";
-
-    text.forEach(letter =>{
-        const span = document.createElement("span");
-        span.className = "letter";
-        span.innerText = letter;
-        element.appendChild(span);
-    });
+window.onload = function(){
+	window.scrollTo(0, 0);
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    split("inst_blast");
-    split("git_blast");
-    split("dis_blast");
-});
-
 function change(){
-	const elements = document.querySelectorAll("#body, h2, #git_blast, #inst_blast, #dis_blast, button");
+	const elements = document.querySelectorAll("#body, h2, button, theme-change, card, hr, btn-style");
 	elements.forEach(element => element.classList.toggle("bright"));
 
 	const body = document.querySelector("body");
 	body.classList.toggle("bright-bg");
-
-	const h1 = document.querySelector("h1");
-	h1.classList.toggle("bright-h1");
 
 	const btn = document.querySelector("button");
 	if(btn.value == 1){
